f9b66e7fa759aae8f39188940edd2aafa73e0331
Dmitriy Monakhov <dimamon6032403@gmail.com>
2025-04-26 14:34:11 +0200

    LineSegment added

---
+++ public/CityDraft/Curves/LineSegment.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "ICurve.h"
+
+namespace CityDraft::Curves
+{
+	class LineSegment: public ICurve
+	{
+	public:
+		LineSegment(const CityDraft::Vector2D& start, const CityDraft::Vector2D& end);
+		LineSegment();
+
+		void SetStart(const CityDraft::Vector2D& start);
+		void SetEnd(const CityDraft::Vector2D& end);
+		void SetPoints(const CityDraft::Vector2D& start, const CityDraft::Vector2D& end);
+
+		const CityDraft::Vector2D& GetStart() const;
+		const CityDraft::Vector2D& GetEnd() const;
+
+		// ICurve
+		CityDraft::Vector2D GetPoint(double t) const override;
+
+		CityDraft::Vector2D GetTangent(double t) const override;
+
+		CityDraft::Vector2D GetNormal(double t) const override
+		{
+			CityDraft::Vector2D tan = GetTangent(t);
+			return {-tan.GetY(), tan.GetX()};
+		}
+
+		double GetLength(double t) const override;
+
+		double GetParameterAtLength(double s) const override;
+
+		double GetClosestParameter(const CityDraft::Vector2D& target) const override;
+
+	private:
+		CityDraft::Vector2D m_Start;
+		CityDraft::Vector2D m_End;
+		double m_Length;
+	};
+}
